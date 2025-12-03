adcf1d06a8e3b57f3338a15629b672b6cd1c21ef
phloi183 <phloi12345674@gmail.com>
2025-12-03 09:33:52 +0700

    Add files via upload

---
+++ QLSV.sql
@@ -0,0 +1,91 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: qlsv
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `diem_sinhvien`
+--
+
+DROP TABLE IF EXISTS `diem_sinhvien`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `diem_sinhvien` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `mssv` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `holot` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ten` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `diem1` float DEFAULT '0',
+  `diem2` float DEFAULT '0',
+  `diem3` float DEFAULT '0',
+  `diemtb` float DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `mssv` (`mssv`),
+  CONSTRAINT `diem_sinhvien_ibfk_1` FOREIGN KEY (`mssv`) REFERENCES `sinhvien` (`mssv`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `diem_sinhvien`
+--
+
+LOCK TABLES `diem_sinhvien` WRITE;
+/*!40000 ALTER TABLE `diem_sinhvien` DISABLE KEYS */;
+/*!40000 ALTER TABLE `diem_sinhvien` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sinhvien`
+--
+
+DROP TABLE IF EXISTS `sinhvien`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sinhvien` (
+  `mssv` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `holot` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ten` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phai` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ngaysinh` date DEFAULT NULL,
+  `lop` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `khoa` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `diem1` float DEFAULT '0',
+  `diem2` float DEFAULT '0',
+  `diem3` float DEFAULT '0',
+  `diemtb` float DEFAULT '0',
+  PRIMARY KEY (`mssv`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sinhvien`
+--
+
+LOCK TABLES `sinhvien` WRITE;
+/*!40000 ALTER TABLE `sinhvien` DISABLE KEYS */;
+INSERT INTO `sinhvien` VALUES ('DPM245451','PHAN THANH','LOI','Nam','2000-01-01','DH25PM','CNTT',9,10,8,9);
+/*!40000 ALTER TABLE `sinhvien` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-03  9:32:54
